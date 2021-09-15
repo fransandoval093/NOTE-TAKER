9e86058050d5d5d38121e0d24ad6d8cbf78c2a5d
fransandoval093 <fransandoval093@gmail.com>
2021-09-14 20:15:32 -0700

    Added DB

---
+++ db/tableData.js
@@ -0,0 +1,12 @@
+// DATA | Below data will hold all of the reserved tables.
+const dbArray = [
+  {
+    customerName: 'Ahmed',
+    customerEmail: 'ahmed@example.com',
+    customerID: 'afhaque89',
+    phoneNumber: '000-000-0000',
+  },
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = dbArray;
